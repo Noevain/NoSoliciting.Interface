b0029857a9eaee62aa8615be53030c3319c41f81
kalilistic <35899782+kalilistic@users.noreply.github.com>
2025-01-04 16:22:10 -0500

    initial commit

---
+++ Interface.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NoSoliciting.Interface {
+    public interface IClassifier : IDisposable {
+        void Initialise(byte[] data);
+
+        string Classify(ushort channel, string message);
+    }
+}
